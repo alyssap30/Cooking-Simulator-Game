03941887acda3fdef58d89ac4b23d5dc7ae1e689
Alyssa <alyssaphelps3@gmail.com>
2026-04-17 17:27:35 +0100

    Dessert Menu functionality + event delegation use

--- script.js
+++ script.js
@@ -17,27 +17,27 @@ const BoostsTab = document.getElementById("boosts-menu");
 
 // Food Tab Variables
 const FoodTab = document.getElementById("food-menu");
-const ItalianFood = document.getElementById("italian-food-menu");
-const EnglishFood = document.getElementById("english-food-menu");
-const FoodMenuSelection = [ItalianFood, EnglishFood];
+const italianFood = document.getElementById("italian-food-menu");
+const englishFood = document.getElementById("english-food-menu");
+const FoodMenuSelection = [italianFood, englishFood];
 const FoodButtons = [document.getElementById("italian-food-button"), document.getElementById("english-food-button")];
 
 // Drink Tab Variables
-const DrinkTab = document.getElementById("drink-menu");
+const drinkTab = document.getElementById("drink-menu");
 const drinksLiquid = document.getElementById("drink-liquid");
-const ColdDrinks = document.getElementById("cold-drinks-menu");
+const coldDrinks = document.getElementById("cold-drinks-menu");
 const coldCup = document.getElementById("drink-glass");
-const HotDrinks = document.getElementById("hot-drinks-menu");
+const hotDrinks = document.getElementById("hot-drinks-menu");
 const hotCup = document.getElementById("drink-cup-top");
-const DrinkMenuSelection = [ColdDrinks, HotDrinks];
-const DrinkButtons = [document.getElementById("cold-drinks-button"), document.getElementById("hot-drinks-button")];
+const drinkMenuSelection = [coldDrinks, hotDrinks];
+const drinkButtons = [document.getElementById("cold-drinks-button"), document.getElementById("hot-drinks-button")];
 
 // Desert Tab Variables
-const DessertsTab = document.getElementById("desserts-menu");
-const ColdDesserts = document.getElementById("cold-desserts-menu");
-const HotDesserts = document.getElementById("hot-desserts-menu");
-const DessertsMenuSelection = [ColdDesserts, HotDesserts];
-const DessertsButtons = [document.getElementById("cold-desserts-button"), document.getElementById("hot-desserts-button")];
+const dessertsTab = document.getElementById("desserts-menu");
+const coldDesserts = document.getElementById("cold-desserts-menu");
+const hotDesserts = document.getElementById("hot-desserts-menu");
+const dessertsMenuSelection = [coldDesserts, hotDesserts];
+const dessertsButtons = [document.getElementById("cold-desserts-button"), document.getElementById("hot-desserts-button")];
 
 // Variables used throughout the program
 const cashStorage = document.getElementById("cash-count");
@@ -46,7 +46,7 @@ let moneyPerSecond = 0;
 let level = 1;
 let xp = 0;
 let requireXpToLevelUp = 50;
-const NavigationMenu = [StaffTab, BoostsTab, FoodTab, DrinkTab, DessertsTab]
+const navigationMenu = [StaffTab, BoostsTab, FoodTab, drinkTab, dessertsTab]
 
 // Saves on Refresh
 document.addEventListener('DOMContentLoaded', function(){
@@ -56,13 +56,13 @@ document.addEventListener('DOMContentLoaded', function(){
    ChefMenu.style.display = "none";
    DessertSpecialistMenu.style.display = "none";
    EnglishFood.style.display = "none";
-   HotDrinks.style.display = "none";
+   hotDrinks.style.display = "none";
    drinkMachine.style.display = "none"
 });
 
 // Navigation Functions
 function Open_menu(tabName) {
-   for (let tab of NavigationMenu) {
+   for (let tab of navigationMenu) {
        if (tab !== tabName) {
            tab.style.display = "none"}
        else
@@ -99,14 +99,10 @@ document.getElementById("food-button").onclick = function() {
    Open_menu(FoodTab)};
 
 document.getElementById("drinks-button").onclick = function() {
-   Open_menu(DrinkTab)};
+   Open_menu(drinkTab)};
 
 document.getElementById("desserts-button").onclick = function() {
-   Open_menu(DessertsTab)};
-document.getElementById("cold-desserts-button").onclick = function() {
-   Tab_switch(ColdDesserts, DessertsButtons, DessertsMenuSelection, document.getElementById("cold-desserts-button"))};
-document.getElementById("hot-desserts-button").onclick = function() {
-   Tab_switch(HotDesserts, DessertsButtons, DessertsMenuSelection, document.getElementById("hot-desserts-button"))};
+   Open_menu(dessertsTab)};
 
 // Money Spending Class
 class Money_Spending {
@@ -131,41 +127,44 @@ class Money_Spending {
       else {
          alert("Not enough cash");
          return false}
-   }
-};
+   }};
 
+// Food Class Declaration
 class Food extends Money_Spending {
    constructor(itemName, itemPrice, buttonID) {
        super(itemName, itemPrice, buttonID);
    }
    Display_foodCounter() {
       pass 
-   }
-};
+   }};
 
  // Food Menu Navigation
 document.getElementById("italian-food-button").onclick = function() {
-   Tab_switch(ItalianFood, FoodButtons, FoodMenuSelection, document.getElementById("italian-food-button"))};
+   Tab_switch(italianFood, FoodButtons, FoodMenuSelection, document.getElementById("italian-food-button"))};
 document.getElementById("english-food-button").onclick = function() {
-   Tab_switch(EnglishFood, FoodButtons, FoodMenuSelection, document.getElementById("english-food-button"))};
+   Tab_switch(englishFood, FoodButtons, FoodMenuSelection, document.getElementById("english-food-button"))};
 
 // Italian Food Selection
 const Pizza = new Food("Pizza", 10, document.getElementById("pizza-button"));
-document.getElementById("pizza-button").onclick = function() {
-   Pizza.Buying_Item()};
-
 const Pasta = new Food("Pasta", 20, document.getElementById("pasta-button"));
-document.getElementById("pasta-button").onclick = function() {
-   Pasta.Buying_Item()};
+const italianItems = {"pizza-button": Pizza, "pasta-button": Pasta}
 
+italianFood.addEventListener('click', function(event) {
+   const clickedButton = event.target
+   let id = clickedButton.id
+   italianItems[id].Buying_Item()
+});
+   
 // English Food Selection
-const FishAndChips = new Food("Fish and Chips", 15, document.getElementById("fish-and-chips-button"));
-document.getElementById("fish-and-chips-button").onclick = function() {
-   FishAndChips.Buying_Item()};
+const fishAndChips = new Food("Fish and Chips", 15, document.getElementById("fish-and-chips-button"));
+const fullEnglish = new Food("Full English Breakfast", 20, document.getElementById("full-english-button"));
+const englishItems = {"fish-and-chips-button": fishAndChips, "full-english-button": fullEnglish}
 
-const FullEnglish = new Food("Full English Breakfast", 20, document.getElementById("full-english-button"));
-document.getElementById("full-english-button").onclick = function() {
-   FullEnglish.Buying_Item()};
+englishFood.addEventListener('click', function(event) {
+   const clickedButton = event.target
+   let id = clickedButton.id
+   englishItems[id].Buying_Item()
+});
 
 // Drink Machine Functions
 class Drinks extends Money_Spending {
@@ -178,7 +177,7 @@ class Drinks extends Money_Spending {
    Drink_Machine() {
       cashRegister.style.display = "none";
       drinkMachine.style.display = "flex";
-      DrinkTab.style.display  = "none";
+      drinkTab.style.display  = "none";
       drinksLiquid.style.backgroundColor = this.drinkColor
       this.percentageFull = 0;
       drinksLiquid.style.height = this.percentageFull + "%";
@@ -208,63 +207,79 @@ class Drinks extends Money_Spending {
       }
    }
 
-// Cold drinks Selection
+// Drinks Menu Navigation 
 document.getElementById("cold-drinks-button").onclick = function() {
-   Tab_switch(ColdDrinks, DrinkButtons, DrinkMenuSelection, document.getElementById("cold-drinks-button"))};
+   Tab_switch(coldDrinks, drinkButtons, drinkMenuSelection, document.getElementById("cold-drinks-button"))};
+document.getElementById("hot-drinks-button").onclick = function() {
+   Tab_switch(hotDrinks, drinkButtons, drinkMenuSelection, document.getElementById("hot-drinks-button"))};
+
+// Drink Machine Fuctionality
 let activeDrink;
-
-const Coke = new Drinks("Coke", 0.5, document.getElementById("coke-button"), "brown", "coldCup");
-document.getElementById("coke-button").onclick = function() {
-   if (Coke.Buying_Item()) {
-      activeDrink = Coke;
-      Coke.Drink_Machine();
-   }};
-
-const Fanta = new Drinks("Fanta", 0.5, document.getElementById("fanta-button"), "orange", "coldCup");
-document.getElementById("fanta-button").onclick = function() {
-   if (Fanta.Buying_Item()) {
-      activeDrink = Fanta;
-      Fanta.Drink_Machine();
-   }};
-
-const DrPepper = new Drinks("Dr Pepper", 1, document.getElementById("dr-pepper-button"), "brown", "coldCup");
-document.getElementById("dr-pepper-button").onclick = function() {
-   if (DrPepper.Buying_Item()) {
-      activeDrink = DrPepper;
-      DrPepper.Drink_Machine();
-   }};
-
-const Water = new Drinks("Water", 0, document.getElementById("water-button"), "lightblue", "coldCup");
-document.getElementById("water-button").onclick = function() {
-   if (Water.Buying_Item()) {
-      activeDrink = Water;
-      Water.Drink_Machine();
-   }};
-
 document.getElementById("pour-drink-button").onclick = function() {
    activeDrink.Pouring_drink()};
 
+// Cold Drinks Selection
+const Coke = new Drinks("Coke", 0.5, document.getElementById("coke-button"), "brown", "coldCup");
+const Fanta = new Drinks("Fanta", 0.5, document.getElementById("fanta-button"), "orange", "coldCup");
+const drPepper = new Drinks("Dr Pepper", 1, document.getElementById("dr-pepper-button"), "brown", "coldCup");
+const Water = new Drinks("Water", 0, document.getElementById("water-button"), "lightblue", "coldCup");
+const coldDrinkItems = {"coke-button": Coke, "fanta-button": Fanta, "dr-pepper-button": drPepper, "water-button": Water};
+
+coldDrinks.addEventListener('click', function(event) {
+   const clickedButton = event.target
+   let id = clickedButton.id
+   activeDrink = coldDrinkItems[id]
+   if (activeDrink.Buying_Item()) {
+      activeDrink.Drink_Machine()
+}});
+
 // Hot Drinks Selection
-document.getElementById("hot-drinks-button").onclick = function() {
-   Tab_switch(HotDrinks, DrinkButtons, DrinkMenuSelection, document.getElementById("hot-drinks-button"))};
-
 const Tea = new Drinks("Tea", 2, document.getElementById("tea-button"), "orange", "hotCup");
-document.getElementById("tea-button").onclick = function() {
-   if (Tea.Buying_Item()) {
-      activeDrink = Tea;
-      Tea.Drink_Machine();
-   }};
-
 const Coffee = new Drinks("Coffee", 2.5, document.getElementById("coffee-button"), "#8B4513", "hotCup");
-document.getElementById("coffee-button").onclick = function() {
-   if (Coffee.Buying_Item()) {
-      activeDrink = Coffee;
-      Coffee.Drink_Machine();
+const hotChocolate = new Drinks("Hot Chocolate", 3, document.getElementById("hot-chocolate-button"), "brown", "hotCup");
+const hotDrinkItems = {"tea-button": Tea, "coffee-button": Coffee, "hot-chocolate-button": hotChocolate}
+
+hotDrinks.addEventListener('click', function(event) {
+   const clickedButton = event.target
+   let id = clickedButton.id
+   activeDrink = hotDrinkItems[id]
+   if (activeDrink.Buying_Item()) {
+      activeDrink.Drink_Machine()
+}});
+
+// Desserts Class Declaration 
+class Dessert extends Money_Spending {
+   constructor(itemName, itemPrice, buttonID) {
+       super(itemName, itemPrice, buttonID);
+   }
+   Display_dessertMachine() {
+      pass
    }};
 
-const HotChocolate = new Drinks("Hot Chocolate", 3, document.getElementById("hot-chocolate-button"), "brown", "hotCup");
-document.getElementById("hot-chocolate-button").onclick = function() {
-   if (HotChocolate.Buying_Item()) {
-      activeDrink = HotChocolate;
-      HotChocolate.Drink_Machine();
-   }};
+// Desserts Menu Navigation
+document.getElementById("cold-desserts-button").onclick = function() {
+   Tab_switch(coldDesserts, dessertsButtons, dessertsMenuSelection, document.getElementById("cold-desserts-button"))};
+document.getElementById("hot-desserts-button").onclick = function() {
+   Tab_switch(hotDesserts, dessertsButtons, dessertsMenuSelection, document.getElementById("hot-desserts-button"))};
+
+// Cold Desserts Selection
+const iceCream = new Dessert("Ice Cream", 2, document.getElementById("ice-cream-button"))
+const cheeseCake = new Dessert("Cheesecake", 10, document.getElementById("cheesecake-button"))
+const coldDessertItems = {"ice-cream-button": iceCream, "cheesecake-button": cheeseCake}
+
+coldDesserts.addEventListener('click', function(event) {
+   const clickedButton = event.target
+   let id = clickedButton.id
+   coldDessertItems[id].Buying_Item()
+});
+
+// Hot Desserts Selection
+const chocolateBrownie = new Dessert("Chocolate Brownie", 5, document.getElementById("chocolate-brownie-button"))
+const applePie = new Dessert("Apple Pie", 6, document.getElementById("apple-pie-button"))
+const hotDessertItems = {"chocolate-brownie-button": chocolateBrownie, "apple-pie-button": applePie}
+
+hotDesserts.addEventListener('click', function(event) {
+   const clickedButton = event.target
+   let id = clickedButton.id
+   hotDessertItems[id].Buying_Item()
+});
